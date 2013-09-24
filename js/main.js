7adf7977ea17f33d7d90cc6be9e7deb87bdff206
guillaumegarcia13 <guillaume.garcia13+codio@gmail.com>
2013-09-24 21:18:45 +0000

    init

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-
 var myApp = angular.module('myApp', []);
-
 $.fn.editable.defaults.mode = 'inline';
 
 function Ctrl($scope, $http) {
@@ -8,44 +6,31 @@ function Ctrl($scope, $http) {
     $scope.person_name = "John Doe";
     $scope.address = "133, North Avenue";
     $scope.total = 0;
-    
-    
-    $scope.persons = JSON.parse(typeof localStorage['data'] == "undefined" ? null: localStorage['data']);
-    
-   if ($scope.persons == null ) {
-    
-$http.get('crm.json').success(function(data) {
-	$scope.persons = data;
-});    
-
-$scope.persons = [];
- 
-   
-  };
-  
-    $scope.newsal = function(a,b) {
-    return parseInt(a) + parseInt(b);
-    };                     
-    
-    $scope.pourc = function(a,b) {
-    return Math.round((parseInt(a) / parseInt(b)) * 10000) / 100 ;
-    };                     
-    
-    
+    $scope.persons = JSON.parse(typeof localStorage['data'] == "undefined" ? null : localStorage['data']);
+    if($scope.persons == null) {
+        $http.get('crm.json').success(function(data) {
+            $scope.persons = data;
+        });
+        $scope.persons = [];
+    };
+    $scope.newsal = function(a, b) {
+        return parseInt(a) + parseInt(b);
+    };
+    $scope.pourc = function(a, b) {
+        return Math.round((parseInt(a) / parseInt(b)) * 10000) / 100;
+    };
     $scope.save = function() {
-    localStorage['data'] = JSON.stringify($scope.persons);
-    };                                 
-
-  };
-
-  myApp.filter("DoTotal", function() {
-  return function(items) {
-    var total = 0, i = 0;
-    for (i = 0; i < items.length; i++) total += parseInt(items[i].ai);
-    return total;
-  }
+        localStorage['data'] = JSON.stringify($scope.persons);
+    };
+};
+myApp.filter("DoTotal", function() {
+    return function(items) {
+        var total = 0,
+            i = 0;
+        for(i = 0; i < items.length; i++) total += parseInt(items[i].ai, 10);
+        return total;
+    };
 });
-
 myApp.directive('xeditable', function($timeout) {
     return {
         restrict: 'A',
@@ -58,7 +43,7 @@ myApp.directive('xeditable', function($timeout) {
                         scope.$apply();
                     }
                 });
-            }
+            };
             $timeout(function() {
                 loadXeditable();
             }, 10);
